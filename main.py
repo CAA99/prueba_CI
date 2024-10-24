6bd72a4e43b0817d58dc747d3bc72073339bc5aa
Cristian Archila <cristian.archila99@gmail.com>
2024-10-24 10:25:19 -0500

    prueba yml

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 def my_function():
-print(  "Hello World")
+    """
+    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+    """
+    print(  "Hello World")
